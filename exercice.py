105c0cc6a3786adfd50739b538bb2b8db9033ca0
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2020-09-17 17:55:20 -0400

    cours

--- exercice.py
+++ exercice.py
@@ -4,19 +4,35 @@
 import random
 
 def is_even_len(string):
-	return 0xDEADBEEF
+	return	len(string) % 2 ==0
+
+
 
 
 def get_num_char(string, char):
-	return 0xDEADBABE
+	num_char = 0
+	for c in string:
+		if c==char:
+			num_char+=1
+	return num_char
+
 
 
 def get_first_part_of_name(name):
-	return ""
+
+	first_part = name.split("-")[0]
+	capitalized =first_part[0].upper()+ first_part[1:].lower()
+	return "Bonjour , "+capitalized
 
 
 def get_random_sentence(animals, adjectives, fruits):
-	return ""
+	basic_sentence=f"Aujourd’hui, j’ai vu un %s s’emparer d’un panier %s plein de %s ."
+
+	animal_world = animals[random.randrange(0,len(animals))]
+	adject_world = adjectives[random.randrange(0, len(adjectives))]
+	fruit_world = fruits[random.randrange(0, len(fruits))]
+	return basic_sentence % (animal_world, adject_world, fruit_world)
+
 
 
 if __name__ == "__main__":
@@ -27,7 +43,7 @@ if __name__ == "__main__":
 	eggs = "Hello, world!"
 	print(f"Le nombre d'occurrence de l dans '{eggs}' est : {get_num_char(eggs, 'l')}.")
 
-	parrot = "jean-marc"
+	parrot = "jEaN-MARCmarc"
 	print(f"Pour {parrot}, on a '{get_first_part_of_name(parrot)}'.")
 
 	animals = ("chevreuil", "chien", "pigeon")
